197d99a55da39a9622693590e417c9ce3fc65ca9
ngxxx307 <ngxxx307@umn.edu>
2022-02-17 10:49:32 +0800

    first commit

--- decisionTree/tree.py
+++ decisionTree/tree.py
@@ -1,1 +1,10 @@
-print('yy')
+from turtle import right
+
+
+class node:
+    def __init__(self, left, right, feature, split, is_leaf):
+        self.left = left
+        self.right = right
+        self.feature = feature
+        self.split = split
+        self.is_leaf = is_leaf
